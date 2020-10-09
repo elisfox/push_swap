a601d11fcc3ce5c433194bb167ab9f79a117ceac
Joyeuse Josephine <>
2020-10-09 20:04:41 +0300

    add max and min

--- src/new_sort.c
+++ src/new_sort.c
@@ -8,21 +8,17 @@ void    first_up(t_massive *s, int count_first)
         s->message = ft_strjoin_f(s->message, "ra\n", 1);
         count_first--;
     }
-    //push_b(s);
-    //s->message = ft_strjoin_f(s->message, "pb\n", 1);
 }
 
-void    first_b_up(t_massive *s, int count_low)
+void    first_b_up(t_massive *s, int count_first)
 {
-    while (count_low)
+    while (count_first)
     {
         s->b = rotate_a_b(s->b);
         s->message = ft_strjoin_f(s->message, "rb\n", 1);
-        count_low--;
+        count_first--;
     }
 }
-
-
 
 void    second_up(t_massive *s, int count_second)
 {
@@ -34,33 +30,32 @@ void    second_up(t_massive *s, int count_second)
     }
     s->a = rr_a_b(s->a);
     s->message = ft_strjoin_f(s->message, "rra\n", 1);
-    //push_b(s);
-    //s->message = ft_strjoin_f(s->message, "pb\n", 1);
 }
 
-void    second_b_up(t_massive *s, int count_high)
+void    second_b_up(t_massive *s, int count_second)
 {
-    while (count_high)
+    while (count_second)
     {
-        s->a = rr_a_b(s->a);
-        s->message = ft_strjoin_f(s->message, "rra\n", 1);
-        count_high--;    
+        s->b = rr_a_b(s->b);
+        s->message = ft_strjoin_f(s->message, "rrb\n", 1);
+        count_second--;    
     }
-    s->a = rr_a_b(s->a);
-    s->message = ft_strjoin_f(s->message, "rra\n", 1);
+    s->b = rr_a_b(s->b);
+    s->message = ft_strjoin_f(s->message, "rrb\n", 1);
 }
 
 
 void    push_to_b(t_massive *s)
 {
     t_stack *tmp;
-    int count;
-    int count_low;
-    int count_high;
+    int count_first;
+    int count_second;
+    int max_less;
 
     tmp = s->b;
-    count_low = 0;
-    count_high = 0;
+    max_less = 0;
+    count_first = 0;
+    count_second = 0;
     if (tmp == NULL)
     {
         push_b(s);
@@ -82,32 +77,45 @@ void    push_to_b(t_massive *s)
         }   
     }
     else
-    {    
-        while(s->a->index < tmp->index)
+    {
+        printf("s->a->index = %d\n", s->a->index);
+        while (tmp)
         {
+            max_less = ((tmp->index > max_less && s->a->index > tmp->index)? tmp->index : max_less);
             tmp = tmp->next;
-            count_low++;
         }
-        count = count_low;
-        
-        while(tmp->next != NULL)
-            tmp = tmp->next;
-        
-        while(s->a->index > tmp->index && tmp->prev)
+        printf("max_less = %d\n", max_less);
+
+        if (max_less != 0)
         {
-            tmp = tmp->prev;
-            count_high++;
+            
+            tmp = s->b;
+            while (tmp->index != max_less)
+            {
+                tmp = tmp->next;
+                count_first++;
+            }
+            printf("count_first = %d\n", count_first);
+            while(tmp->next != NULL)
+                tmp = tmp->next;
+            while (tmp->index != max_less)
+            {
+                tmp = tmp->prev;
+                count_second++;
+            }
+            printf("count_second = %d\n", count_second);
+            if (count_first <= count_second)
+                first_b_up(s, count_first);
+            else
+                second_b_up(s, count_second);
+            push_b(s);
+            s->message = ft_strjoin_f(s->message, "pb\n", 1);
         }
-        printf("!\n");
-        if (count_low <= count_high)
-            first_b_up(s, count_low);
         else
-            second_b_up(s, count_high);
-        push_b(s);
-        if (count_low <= count_high)
-            second_b_up(s, count_low);
-        else
-            first_b_up(s, count_high);
+        {
+            push_b(s);
+            s->message = ft_strjoin_f(s->message, "pb\n", 1);
+        }
     }
 }
 
@@ -132,7 +140,6 @@ void    five_sort(t_massive *s, int size_chunk)
     printf("first = %d\n", first->index);
     while(tmp->next != NULL)
         tmp = tmp->next;
-    printf("last = %d\n", s->a->index);
     while(tmp->index > size_chunk)
     {
         tmp = tmp->prev;
@@ -194,19 +201,18 @@ void    new_sort(t_massive *s)
     size_chunk = s->size;
     count = 0;
     printf("!\n");
-    if (s->size >= 100)
+    if (s->size >= 40)
     {
-        //size_chunk = s->size / 5;
-        size_chunk = 5;
+        size_chunk = s->size / 4;
         printf("size_chunk = %d\n", size_chunk);
         while(count < size_chunk)
         {
             five_sort(s, size_chunk);
             count++;
         }
-        print_stack(s->a,s->b);
+        //print_stack(s->a,s->b);
     }
     //ft_clearstr_stack(s);
-    ft_printf("%s", s->message);
-	free(s->message);
+    //ft_printf("%s", s->message);
+	//free(s->message);
 }
